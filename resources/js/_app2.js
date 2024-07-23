800ae1718c90cb474c9e415cd624fd59db1ad204
RahmatSatyawan <rahmat.stywan@gmail.com>
2024-07-23 15:10:54 +0700

    chore: Add .vercelignore, api/index.php, and resources/js/_app2.js files

--- resources/js/_app2.js
+++ resources/js/_app2.js
@@ -4,19 +4,17 @@ import './bootstrap';
 var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
 var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
-var themeToggleBtn = document.getElementById("theme-toggle");
-
 if (
     localStorage.getItem("color-theme") === "dark" ||
     (!("color-theme" in localStorage) &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
-)  {
+) {
     themeToggleLightIcon.classList.remove("hidden");
-    themeToggleDarkIcon.classList.add("hidden");
-    } else {
+} else {
     themeToggleDarkIcon.classList.remove("hidden");
-    themeToggleLightIcon.classList.add("hidden");
 }
+
+var themeToggleBtn = document.getElementById("theme-toggle");
 
 themeToggleBtn.addEventListener("click", function () {
     themeToggleDarkIcon.classList.toggle("hidden");
@@ -40,3 +38,19 @@ themeToggleBtn.addEventListener("click", function () {
         }
     }
 });
+
+// // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+//     document.documentElement.classList.add('dark')
+//   } else {
+//     document.documentElement.classList.remove('dark')
+//   }
+  
+//   // Whenever the user explicitly chooses light mode
+//   localStorage.theme = 'light'
+  
+//   // Whenever the user explicitly chooses dark mode
+//   localStorage.theme = 'dark'
+  
+//   // Whenever the user explicitly chooses to respect the OS preference
+//   localStorage.removeItem('theme')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,21 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
+      
+      <script>
+            // It's best to inline this in `head` to avoid FOUC (flash of unstyled content) when changing pages or themes
+            if (
+                localStorage.getItem('color-theme') === 'dark' ||
+                (!('color-theme' in localStorage) &&
+                    window.matchMedia('(prefers-color-scheme: dark)').matches)
+            ) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+        </script>
+      
+      <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
